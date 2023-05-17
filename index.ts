fcfe9ec203737a0d2020c4bd3c074a79ebaafa4f
Danil Elfimov <dancingdanil98@gmail.com>
2023-05-17 14:14:10 +0400

    upd

--- index.ts
+++ index.ts
@@ -1,1 +1,1 @@
-export { default as App } from "./src/App"
+export { default as Auth } from "./src/components/Auth"
--- src/App.tsx
+++ src/App.tsx
@@ -1,58 +1,7 @@
-import { ethers } from "ethers"
-import { useEffect, useState } from "react"
 import "./App.scss"
 
-import { getCryptoHash } from "./utils/utils"
-
 function App() {
-  const [email, setEmail] = useState("")
-  const [walletEOA, setWalletEOA] = useState<any>()
-
-  const handleEmailChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setEmail(e.target.value)
-  }
-
-  const handleEnterDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
-    if (e.key === "Enter") {
-      handleAuth()
-    }
-  }
-
-  const handleAuth = async () => {
-    if (email) {
-      const userPrivateKey = await getCryptoHash(email)
-      console.log(`User private key: ${userPrivateKey}`)
-      const newUserWallet = new ethers.Wallet(userPrivateKey)
-      console.log("newUserWallet", newUserWallet)
-      setWalletEOA(newUserWallet)
-      setEmail("")
-    }
-  }
-
-  useEffect(() => {
-    console.log("email", email)
-  }, [email])
-
-  return (
-    <div className="app">
-      <div className="input">
-        <div className="input_label">Enter email</div>
-        <div className="input_form">
-          <input
-            onKeyDown={(e) => handleEnterDown(e)}
-            value={email}
-            type="text"
-            className="input_form_field"
-            onChange={handleEmailChange}
-          />
-          <div className="input_form_btn">
-            <div onClick={handleAuth}>Go</div>
-          </div>
-        </div>
-      </div>
-      {walletEOA && <div className="address">EOA {walletEOA.address}</div>}
-    </div>
-  )
+  return <div className="app"></div>
 }
 
 export default App
