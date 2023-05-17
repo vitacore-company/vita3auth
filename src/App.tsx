ce5e1030940a7c633b0e383ee3f130a919c0b061
Danil Elfimov <dancingdanil98@gmail.com>
2023-05-17 11:22:52 +0400

    cleaning

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,12 @@
 import { ethers } from "ethers"
 import { useEffect, useState } from "react"
-import UserWallet from "./utils/Contracts/UserWallet.json"
-import { mint, transferEther } from "./utils/ABI"
 import "./App.scss"
 
-import {
-  withdrawTokens,
-  withdrawEthers,
-  getCryptoHash,
-  parseBigNum,
-} from "./utils/utils"
-import { vitacoreWallet } from "./utils/constants"
-import { addNewWallet, getWallets } from "./utils/API"
+import { getCryptoHash } from "./utils/utils"
 
 function App() {
   const [email, setEmail] = useState("")
   const [walletEOA, setWalletEOA] = useState<any>()
-  const [walletSmart, setWalletSmart] = useState<any>(null)
 
   const handleEmailChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setEmail(e.target.value)
@@ -30,62 +20,18 @@ function App() {
 
   const handleAuth = async () => {
     if (email) {
-      setWalletSmart(null)
       const userPrivateKey = await getCryptoHash(email)
       console.log(`User private key: ${userPrivateKey}`)
       const newUserWallet = new ethers.Wallet(userPrivateKey)
       console.log("newUserWallet", newUserWallet)
       setWalletEOA(newUserWallet)
       setEmail("")
-      const data = await getWallets()
-      console.log("data", data)
-      const walletSmartMatch = data.find(
-        (obj: any) => obj[newUserWallet.address]
-      )
-      if (walletSmartMatch) {
-        const userContractAddress = walletSmartMatch[newUserWallet.address]
-        const userContract = new ethers.Contract(
-          userContractAddress,
-          UserWallet.abi,
-          vitacoreWallet
-        )
-        setWalletSmart(userContract)
-      }
-    }
-  }
-
-  const deploy = async () => {
-    console.log("deploy started")
-    const factory = new ethers.ContractFactory(
-      UserWallet.abi,
-      UserWallet.bytecode,
-      vitacoreWallet
-    )
-
-    const contract = await factory.deploy(walletEOA.address)
-    console.log("walletSmart", contract.address)
-    setWalletSmart(contract)
-    await addNewWallet(walletEOA.address, contract.address)
-    console.log("deploy finished")
-    return contract.address
-  }
-
-  const transferAssets = async (amount: string, transfer: any) => {
-    if (walletSmart) {
-      transfer(amount, walletSmart.address)
-    } else {
-      const smartAddress = await deploy()
-      transfer(amount, smartAddress)
     }
   }
 
   useEffect(() => {
     console.log("email", email)
   }, [email])
-
-  useEffect(() => {
-    console.log("walletSmart", walletSmart)
-  }, [walletSmart])
 
   return (
     <div className="app">
@@ -103,45 +49,8 @@ function App() {
             <div onClick={handleAuth}>Go</div>
           </div>
         </div>
-        <div className="test">
-          <div className="test_btn" onClick={() => transferAssets("100", mint)}>
-            Mint
-          </div>
-          <div
-            className="test_btn"
-            onClick={() => transferAssets("0.1", transferEther)}
-          >
-            SendETH
-          </div>
-        </div>
       </div>
-      {walletEOA && (
-        <>
-          <div className="transaction">
-            <div
-              className="transaction_btn"
-              onClick={() =>
-                withdrawTokens(parseBigNum(10), walletSmart, walletEOA)
-              }
-            >
-              Withdraw tokens
-            </div>
-            <div
-              className="transaction_btn"
-              style={{ marginLeft: "200px" }}
-              onClick={() =>
-                withdrawEthers(parseBigNum(0.1), walletSmart, walletEOA)
-              }
-            >
-              Withdraw ethers
-            </div>
-          </div>
-          <div className="address">EOA {walletEOA.address}</div>
-          <div className="address-right">
-            Smart {walletSmart?.address || ""}
-          </div>
-        </>
-      )}
+      {walletEOA && <div className="address">EOA {walletEOA.address}</div>}
     </div>
   )
 }
--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,7 +1,7 @@
 import { ethers } from "ethers"
 import { sha256 } from "crypto-hash"
 
-import { provider, vita20Address, vitacoreWallet } from "./constants"
+import { provider } from "./constants"
 
 export function createWallet(privateKey: string) {
   const userWallet = new ethers.Wallet(privateKey)
@@ -9,56 +9,7 @@ export function createWallet(privateKey: string) {
   return connectedWallet
 }
 
-export const getSignature = async (
-  amount: string,
-  nonce: number,
-  walletSmart: any,
-  walletEOA: any
-) => {
-  const hash = ethers.utils.solidityKeccak256(
-    ["address", "uint256", "uint256", "address"],
-    [vitacoreWallet.address, amount, nonce, walletSmart.address]
-  )
-
-  const messageHashBin = ethers.utils.arrayify(hash)
-  const signature = await walletEOA.signMessage(messageHashBin)
-  return signature
-}
-
-export const nonce = 26
-
-export const withdrawTokens = async (
-  amount: string,
-  walletSmart: any,
-  walletEOA: any
-) => {
-  console.log("transfer started")
-  const signature = await getSignature(amount, nonce, walletSmart, walletEOA)
-  const txHash = await walletSmart.withdrawToken(
-    vita20Address,
-    amount,
-    nonce,
-    signature
-  )
-  await txHash.wait()
-  console.log("transfer finished")
-}
-
-export const withdrawEthers = async (
-  amount: string,
-  walletSmart: any,
-  walletEOA: any
-) => {
-  console.log("transfer started")
-  const signature = await getSignature(amount, nonce, walletSmart, walletEOA)
-  const txHash = await walletSmart.withdrawEth(amount, nonce, signature)
-  await txHash.wait()
-  console.log("transfer finished")
-}
-
 export const getCryptoHash = async (email: string) => {
   const cryptoHash = await sha256(email)
   return cryptoHash
 }
-
-export const parseBigNum = (num: number) => (num * 10 ** 18).toString()
