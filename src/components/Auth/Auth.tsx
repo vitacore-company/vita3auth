bca6f584aaa787d6ce690f96ce029306d7f52e7a
Danil Elfimov <dancingdanil98@gmail.com>
2023-05-31 18:17:41 +0400

    version up

--- src/components/Auth/Auth.tsx
+++ src/components/Auth/Auth.tsx
@@ -41,7 +41,7 @@ function Auth(props: AuthI) {
 
   const handleAuth = async () => {
     if (email && password) {
-      console.log("auth start")
+      console.log("auth")
       const userPrivateKey = await getCryptoHash(`${email}_${password}`)
       console.log(`User private key: ${userPrivateKey}`)
       const newUserWallet = new ethers.Wallet(userPrivateKey)
