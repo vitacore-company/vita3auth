232b31aaf1f2aac747320905f5244e97199443d9
Danil Elfimov <dancingdanil98@gmail.com>
2023-06-16 10:10:59 +0300

    version up

--- src/components/AddressEOA/AddressEOA.tsx
+++ src/components/AddressEOA/AddressEOA.tsx
@@ -2,6 +2,7 @@ import { IAddressEOA } from "../../types"
 import { ReactComponent as MetamaskLogo } from "../../assets/metamask.svg"
 import { ReactComponent as CopyLogo } from "../../assets/copy.svg"
 import { parseAccount } from "../../utils/utils"
+import { Tooltip } from "react-tooltip"
 
 const AddressEOA = (props: IAddressEOA) => {
   const { address, noAddressLabel } = props
@@ -14,9 +15,22 @@ const AddressEOA = (props: IAddressEOA) => {
     <div className="address">
       <MetamaskLogo />
       <div className="address_number">{address && parseAccount(address)}</div>
-      <div className="address_copy" onClick={copy}>
+      <div
+        data-tooltip-id="copy-tooltip"
+        data-tooltip-content="copy"
+        className="address_copy"
+        onClick={copy}
+      >
         <CopyLogo />
       </div>
+      <Tooltip
+        id="copy-tooltip"
+        style={{
+          backgroundColor: "rgba(39, 41, 39, 0.681)",
+          color: "#ffffff",
+          borderRadius: "15px",
+        }}
+      />
     </div>
   ) : (
     <div className="address-no">{noAddressLabel || "No Wallet"}</div>
