54fa19f9329e2ef833ed437f4ec389d32d168c3c
Danil Elfimov <dancingdanil98@gmail.com>
2023-06-18 23:35:07 +0300

    version up

--- src/components/AddressEOA/AddressEOA.tsx
+++ src/components/AddressEOA/AddressEOA.tsx
@@ -3,25 +3,39 @@ import { ReactComponent as MetamaskLogo } from "../../assets/metamask.svg"
 import { ReactComponent as CopyLogo } from "../../assets/copy.svg"
 import { parseAccount } from "../../utils/utils"
 import { Tooltip } from "react-tooltip"
+import { useEffect, useState } from "react"
 
 const AddressEOA = (props: IAddressEOA) => {
   const { address, noAddressLabel } = props
+  const [copied, setCopied] = useState(false)
 
   const copy = async () => {
     await navigator.clipboard.writeText(address || "")
+    setCopied(true)
   }
 
+  useEffect(() => {
+    if (copied) {
+      setTimeout(() => {
+        setCopied(false)
+      }, 1000)
+    }
+  }, [copied])
+
   return address ? (
-    <div className="address">
-      <MetamaskLogo />
-      <div className="address_number">{address && parseAccount(address)}</div>
-      <div
-        data-tooltip-id="copy-tooltip"
-        data-tooltip-content="copy"
-        className="address_copy"
-        onClick={copy}
-      >
-        <CopyLogo />
+    <>
+      <div className="address">
+        {copied && <div className="address_copied">COPIED</div>}
+        <MetamaskLogo />
+        <div className="address_number">{address && parseAccount(address)}</div>
+        <div
+          data-tooltip-id="copy-tooltip"
+          data-tooltip-content="copy"
+          className="address_copy"
+          onClick={copy}
+        >
+          <CopyLogo />
+        </div>
       </div>
       <Tooltip
         id="copy-tooltip"
@@ -31,7 +45,7 @@ const AddressEOA = (props: IAddressEOA) => {
           borderRadius: "15px",
         }}
       />
-    </div>
+    </>
   ) : (
     <div className="address-no">{noAddressLabel || "No Wallet"}</div>
   )
